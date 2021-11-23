b3ed9a061b85b60303cb7be8bc1893749bae3d10
Drashty <drashtymajmudar@gmail.com>
2021-11-22 18:58:04 -0800

    first commit

---
+++ app/src/main/java/com/example/plicoapp/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.plicoapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Button login, toSignUp;
+    private EditText et_email, et_pass;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.btn_login);
+        toSignUp = findViewById(R.id.link_signup);
+
+        et_email = findViewById(R.id.input_email);
+        et_pass = findViewById(R.id.input_password);
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(this, SwipeActivity.class);
+            startActivity(intent);
+        }
+    }
+
+     */
+
+    public void Login(View view) {
+        String email = et_email.getText().toString().trim();
+        String password = et_pass.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(MainActivity.this, SwipeActivity.class);
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    public void Register(View view) {
+        Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/plicoapp/RegisterActivity.java
@@ -0,0 +1,68 @@
+package com.example.plicoapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Button login, toSignUp;
+    private EditText et_email, et_pass, et_username;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registerbasic_info);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        et_email = findViewById(R.id.input_email);
+        et_username = findViewById(R.id.input_username);
+        et_pass = findViewById(R.id.input_password);
+    }
+
+    public void onLoginClicked(View view) {
+        String email = et_email.getText().toString().trim();
+        String password = et_pass.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+
+    }
+
+    public void gotoLogin(View view) {
+        Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(i);
+    }
+}
